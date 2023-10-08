db8b0396f903fc9cf5e227e81d0927350378e92b
xAdvitya <advityasharma21@gmail.com>
2023-10-08 23:28:44 +0530

    updating column in db

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,15 +1,18 @@
-'use client'
+'use client';
 import { useEffect } from 'react';
 import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import { useBoardStore } from '@/store/BoardStore';
 import Column from './Column';
 
 function Board() {
-  const [board, getBoard,setBoardState] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-    state.setBoardState
-  ]);
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  );
 
   useEffect(() => {
     getBoard();
@@ -19,21 +22,70 @@ function Board() {
 
     if (!destination) return;
 
-    if (type === "column") {
+    if (type === 'column') {
       const entries = Object.entries(board.columns);
-      const [removed] = entries.splice(source.index,1);
-      entries.splice(destination.index,0,removed);
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
       const rearrangedColumns = new Map(entries);
-      setBoardState({...board,columns:rearrangedColumns,})
+      setBoardState({ ...board, columns: rearrangedColumns });
+    }
+
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      newTodos.splice(destination.index, 0);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(startCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+      updateTodoInDB(todoMoved, finishCol.id);
+      setBoardState({ ...board, columns: newColumns });
     }
   };
 
-
-  console.log("board",board.columns.entries());
+  console.log('board', board.columns.entries());
 
   return (
     // <h1>board</h1>
-<DragDropContext onDragEnd={handelOnDragEnd}>
+    <DragDropContext onDragEnd={handelOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
         {(provided) => (
           <div
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,10 +1,12 @@
 import { create } from 'zustand';
 import { getTodosGroupedByColumn } from '@/lib/getTodosGroupedByColumn';
+import { databases } from '@/appwrite';
 
 interface BoardState {
   board: Board;
   getBoard: () => void;
   setBoardState: (board: Board) => void; // Add setBoardState function
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -14,4 +16,16 @@ export const useBoardStore = create<BoardState>((set) => ({
     set({ board });
   },
   setBoardState: (board: Board) => set({ board }),
+
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        staus: columnId,
+      }
+    );
+  },
 }));
